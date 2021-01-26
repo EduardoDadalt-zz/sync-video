366ea86d228bdec92fdcb6f496fcca5c2343152c
EduardoDadalt <eduardodadalt1@gmail.com>
2021-01-26 16:53:22 -0300

    2.1

--- pages/api/sendSrc.tsx
+++ pages/api/sendSrc.tsx
@@ -19,49 +19,46 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           args: chromium.args,
           defaultViewport: chromium.defaultViewport,
           executablePath:
-            (await chromium.executablePath) ??
-            "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+            (await chromium.executablePath) ?? "/usr/bin/google-chrome-stable",
           headless: chromium.headless,
           ignoreHTTPSErrors: true,
         });
+        console.log("Abriu o chromez");
       }
       let page = await browser.newPage();
       console.log("Pagina Abriu");
       await page.setRequestInterception(true);
-      // new Promise((resolve, reject) => {
-      //   page.on("request", (req) => {
-      //     const type = req.resourceType();
-      //     if (type === "media") resolve(req.url());
-      //     if (acceptTypes.includes(type)) req.continue();
-      //     else req.abort();
-      //   });
-      //   setTimeout(reject, timer * 1000);
-      // })
-      //   .then(async (src) => {
-      //     console.log({ src });
-      //     if (!!src)
-      //       await firestore.collection("video").doc(path).set({
-      //         currentTime: 0,
-      //         date: Date.now(),
-      //         src,
-      //         play: false,
-      //       });
-      //     statusCode = 200;
-      //   })
-      //   .catch(() => {
-      //     statusCode=404
-      //   })
-      //   .finally(() => page.close());
+      new Promise((resolve, reject) => {
+        page.on("request", async (req) => {
+          try {
+            const type = req.resourceType();
+            if (type === "media") resolve(req.url());
+            if (acceptTypes.includes(type)) req.continue();
+            else req.abort();
+          } catch (e) {}
+        });
+        setTimeout(reject, timer * 1000);
+      })
+        .then(async (src) => {
+          console.log("Vídeo encontrado");
+          if (!!src) {
+            await firestore.collection("video").doc(path).set({
+              currentTime: 0,
+              date: Date.now(),
+              src,
+              play: false,
+            });
+            statusCode = 200;
+          } else statusCode = 404;
+        })
+        .catch(() => (statusCode = 404))
+        .finally(() => page.close().then((e) => console.log("Página Fechou")));
       await page.goto(url);
-      await page.waitForNavigation();
-      await page.close();
-      console.log("Pagina Fechou");
-      statusCode = 200;
     }
     statusCode = 400;
   } catch (error) {
     statusCode = 500;
-    console.error(error);
+    // console.error(error);
   } finally {
     res.statusCode = statusCode;
     res.end();
